3f29e78e916f85c78a40c86ccf7eca5674baa149
stefanJ94 <jovanovic.stefan94@yahoo.com>
2023-11-20 12:56:54 +0100

    First commit

---
+++ src/main/java/com/example/PhonebookManager/PhonebookManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhonebookManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookManagerApplication.class, args);
+	}
+
+}
